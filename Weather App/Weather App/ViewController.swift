6c76a792d872cd5a1273dffc1fc017aa3f0cb623
Aurlin Dhillon <aurlindhillon@gmail.com>
2019-05-07 01:25:37 -0700

    initializing
    initialization of project

---
+++ Weather App/Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Aurlin Dhillon on 2019-05-07.
+//  Copyright © 2019 Aurlin Dhillon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
